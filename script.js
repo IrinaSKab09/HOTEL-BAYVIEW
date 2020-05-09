cf2d31d25c9f2fe1c055e43572ccd9d89c9919db
Irina <55109138+IrinaSKab@users.noreply.github.com>
2020-05-09 20:24:59 +0300

    fix adaptive layout, add parallax

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ navigationMobil.addEventListener('click', function() {
   navigationMobil.classList.toggle ('navigation__mobil-active');
   menu.classList.toggle ('navigation-active');
 })
+
+$(document).ready(function(){
+  $window = $(window);
+$('div[data-type="background"]').each(function(){
+   var $bgobj = $(this);
+$(window).scroll(function() {
+      var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+      var coords = '50%'+ yPos + 'px';
+      $bgobj.css({ backgroundPosition: coords });
+  }); 
+});    
+}); 
